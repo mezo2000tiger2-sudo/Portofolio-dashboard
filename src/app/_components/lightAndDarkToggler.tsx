e376518dc3ff59efcc4abbc0bf2406896204272a
Mustafa Ibrahim <mezo2000tiger2@gmail.com>
2026-04-28 16:33:59 +0300

    fix sun bg color

--- src/app/_components/lightAndDarkToggler.tsx
+++ src/app/_components/lightAndDarkToggler.tsx
@@ -18,7 +18,7 @@ export function LightAndDarkToggler() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size={null} className="bg-slate-700 hover:bg-slate-600 border-0 mr-5 p-2">
+        <Button variant="outline" size={null} className="border-2 border-slate-300 border-solid mr-5 p-2">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
